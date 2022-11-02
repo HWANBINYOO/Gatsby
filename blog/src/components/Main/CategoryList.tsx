e11c86e22c7e9791f5c487c332fb15a1572c6c74
hawnbin <dngh0825@naver.com>
2022-11-02 13:52:25 +0900

    Add Post Item

---
+++ blog/src/components/Main/CategoryList.tsx
@@ -0,0 +1,65 @@
+import React, { FunctionComponent, ReactNode } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+type CategoryItemProps = {
+    active: boolean;
+  }
+
+type GatsbyLinkProps = {
+    children: ReactNode;
+    className?: string;
+    to: string;
+} & CategoryItemProps
+
+export type CategoryListProps = {
+  selectedCategory: string
+  categoryList: {
+    // 프로퍼티 이름은 문자열, 프로퍼티 값은 숫자임을 나타내는 타입 표기 방법
+    [key: string]: number
+  }
+}
+
+const CategoryListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 768px;
+  margin: 100px auto 0;
+`
+
+const CategoryItem = styled(({ active, ...props } : GatsbyLinkProps) => (
+    <Link {...props} />
+  ))`
+    margin-right: 20px;
+    padding: 5px 0;
+    font-size: 18px;
+    font-weight: ${({ active }) => (active ? '800' : '400')};
+    cursor: pointer;
+  
+    &:last-of-type {
+      margin-right: 0;
+    }
+  `
+  
+
+//Object.prototype.entries 메서드는 객체의 열거 가능한 속성들을 [key, value] 쌍의 값들을 가진 배열을 반환하는 기능을 가지고 있다
+const CategoryList: FunctionComponent<CategoryListProps> = function ({
+  selectedCategory,
+  categoryList,
+}) {
+  return (
+    <CategoryListWrapper>
+      {Object.entries(categoryList).map(([name, count]) => (
+        <CategoryItem
+        to={`/?category=${name}`}
+        active={name === selectedCategory}
+        key={name}
+      >
+        #{name}({count})
+      </CategoryItem>
+      ))}
+    </CategoryListWrapper>
+  )
+}
+
+export default CategoryList

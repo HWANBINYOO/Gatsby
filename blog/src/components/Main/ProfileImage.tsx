617846a8bf6e4aade73ba147fc66912104ac65cc
hawnbin <dngh0825@naver.com>
2022-11-02 10:27:29 +0900

    Add Protile  , Footer

---
+++ blog/src/components/Main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
+const PROFILE_IMAGE_LINK =
+  'https://newsimg.sedaily.com/2019/05/22/1VJ8PU7XCR_5.jpg'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
